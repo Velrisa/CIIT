b5ae042b459bd94d78efbf1927d9a6f9e67e3d42
Velrisa <127907584+Velrisa@users.noreply.github.com>
2023-08-09 01:49:42 -0700

    Delete cliente directory

--- cliente/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from './axios.js';
-
-export const pedirRegistro = (user) => axios.post(`/registro`, user);
-export const accederPlataforma = (user) => axios.post(`/acceder`, user);
-export const verificarPeticionToken = () => axios.get('/verificar');
-export const verPerfil = () => axios.get('/perfil');
-
--- cliente/src/resources/codigoClase.js
+++
@@ -1,8 +0,0 @@
-const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const length = 7;
-
-export const codigoClase = () => {
-    var resultado = '';
-    for (var i = length; i > 0; --i) resultado += chars[Math.round(Math.random() * (chars.length - 1))];
-    return resultado;
-}
